3e7a0bd99745f2179d106214b345372ab133de03
Antoine Nauleau <ajnauleau@gmail.com>
2021-03-13 15:41:15 +0100

    Created Entity

--- LoanerApp/Loaner/Model/Loanee.swift
+++ LoanerApp/Loaner/Model/Loanee.swift
@@ -1,20 +1,38 @@
-//
-//  Loanee.swift
-//  loaner
-//
-//  Created by Thomas Vandegriff on 3/13/19.
-//  Copyright © 2019 LinnierGames. All rights reserved.
-//
-
 import UIKit
 
-class Loanee: NSObject {
+class Loanee: NSObject, NSCoding {
 
-        var name: String
-        var contactNumber: String?
-        
-        init(name: String, contactNumber: String?) {
-            self.name = name
-            self.contactNumber = contactNumber
-        }
+    var name: String
+    var contactNumber: String?
+
+    // Protocol requires having Keys for our variables
+    enum Keys: String {
+        case name = "name"
+        case contactNumber = "contactNumber"
+    }
+
+    init(name: String, contactNumber: String?) {
+
+        /** For Future Feature: Ability to access Contacts app:
+         init(name: String, profileImage: UIImage, contactNumber: String?) {
+
+         self.profileImage = profileImage
+         **/
+
+        self.name = name
+        self.contactNumber = contactNumber
+    }
+
+    // Protocol requires an encode method to encode our variables based on the Key
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: "name")
+        aCoder.encode(contactNumber, forKey: "contactNumber")
+    }
+
+    // Protocol requires an init method which acts as a decoder for our variables based on the Key
+    required init?(coder aDecoder: NSCoder) {
+        name = aDecoder.decodeObject(forKey: "name") as! String
+        contactNumber = aDecoder.decodeObject(forKey: "contactNumber") as? String
+        super.init()
+    }
 }
